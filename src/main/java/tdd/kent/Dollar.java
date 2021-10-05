e7f69fa27c64e36d84cc1579b7d752b88c0e8b32
oinsio <oinsio.srv@gmail.com>
2021-10-05 23:33:58 +0300

    refactoring code duplication from Dollar/Franc to Money

--- src/main/java/tdd/kent/Dollar.java
+++
@@ -1,9 +0,0 @@
-package tdd.kent;
-
-public class Dollar extends Money {
-
-    public Dollar(int amount, String currency) {
-
-        super(amount, currency);
-    }
-}
--- src/test/groovy/tdd/kent/MoneyTest.groovy
+++ src/test/groovy/tdd/kent/MoneyTest.groovy
@@ -4,13 +4,20 @@ import spock.lang.Specification
 
 class MoneyTest extends Specification {
 
+    def "should be multiplicative"() {
+
+        given:
+            def five = Money.dollar(5)
+        expect:
+            Money.dollar(10) == five.times(2)
+            Money.dollar(15) == five.times(3)
+    }
+
     def "should be able to equal"() {
 
         expect:
             Money.dollar(5) == Money.dollar(5)
             Money.dollar(5) != Money.dollar(6)
-            Money.franc(5) == Money.franc(5)
-            Money.franc(5) != Money.franc(6)
             !Money.franc(5).equals(Money.dollar(5))
     }
 
@@ -18,10 +25,5 @@ class MoneyTest extends Specification {
         expect:
             "USD" == Money.dollar(1).currency()
             "CHF" == Money.franc(1).currency()
-    }
-
-    def "should equal different classes"() {
-        expect:
-            new Money(10, "CHF") == new Franc(10, "CHF")
     }
 }
